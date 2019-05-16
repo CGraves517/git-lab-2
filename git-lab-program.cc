9dd58158df843ee22f531b29952d49a70af4e7fe
CGraves517 <cg659517@ohio.edu>
2019-05-16 18:12:04 -0400

    First commit

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*        File: git-lab-program.cc
+*      Author: Chandler Graves
+*        Date: 16 May 2019
+* Description:  Prints "Hello Git!"
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    /*add code*/
+    cout << "Hello Git!" << endl;
+    return 0;
+}
